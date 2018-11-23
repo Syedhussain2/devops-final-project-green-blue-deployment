389e15fad06cfe50d194ebdde006d84f5df3ddc4
Tech-A07 <rlecoutre@spartaglobal.com>
2018-11-23 15:12:23 +0000

    test3

--- variables.tf
+++ variables.tf
@@ -3,8 +3,8 @@ variable "vpc_id" {
 }
 
 variable "app_ami_id" {
-  default = "ami-03b715834d64be8c0"
-  # default = "ami-055c1755b888344f7"
+  # default = "ami-03b715834d64be8c0"
+  default = "ami-055c1755b888344f7"
 }
 variable "db_ami_id" {
   default = "ami-052d4b45126cc68ec"
